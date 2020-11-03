265c7d694ae7e2c14be2ec1e3f9178f9640228a7
Waylon Carrier Nunes <wayloncn@gmail.com>
2020-11-03 08:39:05 -0500

    - fixed issues with browser cross-compatibility - implemented screen greyout when header menu is open - changed the way I handled responsive images

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
   private headerWrapperEl: HTMLElement;
   private menuOpen = false;
+  private menuList: HTMLUListElement;
 
   constructor() {}
 
@@ -15,9 +16,35 @@ export class HeaderComponent implements OnInit {
     this.headerWrapperEl = document.querySelector(
       '#headerWrapper'
     ) as HTMLElement;
+
+    this.menuList = this.headerWrapperEl.querySelector(
+      'ul'
+    ) as HTMLUListElement;
+
+    window.addEventListener('click', (event) => {
+      if (!this.menuList.contains(<Node>event.target)) {
+        this.headerWrapperEl.classList.remove('menuOpen');
+        this.menuOpen = false;
+      }
+    });
+
+    let menuToggle = this.headerWrapperEl.querySelector(
+      '#menuToggle'
+    ) as HTMLButtonElement;
+
+    window.addEventListener('touchstart', (event) => {
+      if (
+        !this.menuList.contains(<Node>event.target) &&
+        !menuToggle.contains(<Node>event.target)
+      ) {
+        this.headerWrapperEl.classList.remove('menuOpen');
+        this.menuOpen = false;
+      }
+    });
   }
 
-  public toggleMenu() {
+  public toggleMenu(event: MouseEvent) {
+    event.stopPropagation();
     if (this.menuOpen) {
       this.headerWrapperEl.classList.remove('menuOpen');
     } else {
